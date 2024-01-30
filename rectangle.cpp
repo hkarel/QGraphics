7a2a0b306c426cffe4353819eb55eb130fa9adb6
Pavel Karelin <hkarel@yandex.ru>
2024-01-30 18:59:09 +0300

    Init-коммит

---
+++ rectangle.cpp
@@ -0,0 +1,93 @@
+#include "rectangle.h"
+#include "drag_circle.h"
+#include <QtGui>
+
+namespace qgraph {
+
+Rectangle::Rectangle(QGraphicsScene* scene)
+{
+    setFlags(ItemIsMovable);
+    QPen pen = this->pen();
+    pen.setColor(QColor(0, 255, 0));
+    pen.setWidth(2);
+    setPen(pen);
+
+    setZLevel(1);
+    scene->addItem(this);
+
+    _circle = new DragCircle(scene);
+    _circle->setParent(this);
+    _circle->setVisible(false);
+}
+
+void Rectangle::setFrameScale(float newScale)
+{
+    float s = newScale / frameScale();
+    QRectF r;
+    r.setLeft(0);
+    r.setTop (0);
+    r.setWidth (rect().width()  * s);
+    r.setHeight(rect().height() * s);
+
+    prepareGeometryChange();
+    setRect(r);
+    setPos(pos().x() * s, pos().y() * s);
+    _circle->setPos(r.width(), r.height());
+
+    _frameScale = newScale;
+    //changeSignal.emit_(this);
+}
+
+void Rectangle::dragCircleMove(DragCircle* circle)
+{
+    QRectF r = rect();
+    r.setWidth(circle->x());
+    r.setHeight(circle->y());
+
+    prepareGeometryChange();
+    setRect(r);
+
+    circleMoveSignal.emit_(this);
+}
+
+void Rectangle::dragCircleRelease(DragCircle* circle)
+{
+    if (circle->parentItem() == this)
+    {
+        //circle->setPos(rect().width(), rect().height());
+        circleReleaseSignal.emit_(this);
+    }
+}
+
+QRectF Rectangle::realSceneRect() const
+{
+    float s = frameScale();
+    QRectF r;
+    r.setLeft(pos().x() / s);
+    r.setTop (pos().y() / s);
+    r.setWidth (rect().width()  / s);
+    r.setHeight(rect().height() / s);
+    return r;
+}
+
+void Rectangle::setRealSceneRect(const QRectF& r)
+{
+    float s = frameScale();
+    QPointF pos;
+    pos.setX(r.left() * s);
+    pos.setY(r.top()  * s);
+
+    QRectF rect;
+    rect.setLeft(0);
+    rect.setTop(0);
+    rect.setWidth (r.width()  * s);
+    rect.setHeight(r.height() * s);
+
+    prepareGeometryChange();
+    setRect(rect);
+    setPos(pos.x(), pos.y());
+    _circle->setPos(rect.width(), rect.height());
+}
+
+
+} // namespace qgraph
